b1e67e1ff188f7103142766dda4002cfe007cb05
ajgoward <ajgoward@gmail.com>
2020-06-29 12:57:20 +0000

    finished the timeline and added one project photo

--- timeline.js
+++ timeline.js
@@ -1,8 +1,35 @@
 function hideShow() {
-  var x = document.getElementById("myDIV");
+  var x = document.getElementById("school");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
-  }
-}
+  }}
+
+
+function hideShow1() {
+  var x = document.getElementById("riverside");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }}
+
+
+
+function hideShow2() {
+  var x = document.getElementById("college");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }}
+
+  function hideShow3() {
+  var x = document.getElementById("hollybush");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }}
+
