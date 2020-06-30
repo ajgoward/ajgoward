dee19eabdc58e311a76fb95681061b618b62602d
ajgoward <ajgoward@gmail.com>
2020-06-30 11:16:00 +0000

    sorted out projects

--- timeline.js
+++ timeline.js
@@ -32,23 +32,15 @@ function hideShow2() {
   } else {
     x.style.display = "none";
   }}
-
-var myform = $("form#myform");
-myform.submit(function(event){
-	event.preventDefault();
-
-  // Change to your service ID, or keep using the default service
-  var service_id = "default_service";
-  var template_id = "emal";
-
-  myform.find("button").text("Sending...");
-  emailjs.sendForm(service_id,template_id,myform[0])
-  	.then(function(){ 
-    	alert("Sent!");
-       myform.find("button").text("Send");
-    }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-       myform.find("button").text("Send");
-    });
-  return false;
-});
+ (function(){
+      emailjs.init("user_Bw0wFLIW8MS0xjexXyTL8");
+   })();
+   
+window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate the contact number value
+                this.contact_number.value = Math.random() * 100000 | 0;
+                emailjs.sendForm('contact_service', 'contact_form', this);
+            });
+        }
