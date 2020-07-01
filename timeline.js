83607362baa478ba5cdca7ecb9fd2735fabfac25
ajgoward <ajgoward@gmail.com>
2020-07-01 10:49:59 +0000

    finished my resume

--- timeline.js
+++ timeline.js
@@ -31,16 +31,6 @@ function hideShow2() {
     x.style.display = "block";
   } else {
     x.style.display = "none";
-  }}
- (function(){
-      emailjs.init("user_Bw0wFLIW8MS0xjexXyTL8");
-   })();
-   
-window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // generate the contact number value
-                this.contact_number.value = Math.random() * 100000 | 0;
-                emailjs.sendForm('contact_service', 'contact_form', this);
-            });
-        }
+  }};
+
+  
